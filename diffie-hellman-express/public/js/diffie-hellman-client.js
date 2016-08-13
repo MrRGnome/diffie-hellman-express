4760bca9c365a3163c6de02a690966bd90247548
Austin Fay <austin.fay@alltechnologysolutions.com>
2016-08-12 21:04:04 -0600

    initial commit

---
+++ diffie-hellman-express/public/js/diffie-hellman-client.js
@@ -0,0 +1,18 @@
+﻿var crypto = require('crypto');
+
+$('#log').append('<p>' + Date.now() + ': Loaded crypto module via browserify, calling crypto.createDiffieHellman() with server prime and generator</p>');
+var dh = crypto.createDiffieHellman(sessionStorage['prime'], 'hex', sessionStorage['generator'], 'hex');
+
+$('#log').append('<p>' + Date.now() + ': Created Diffie Hellman object, calling generateKeys</p>');
+sessionStorage['clientPubKey'] = dh.generateKeys('hex');
+
+$('#log').append('<p>' + Date.now() + ': Keys generated! Computing secret</p>');
+sessionStorage['secret'] = dh.computeSecret(sessionStorage['serverPubKey'], 'hex', 'hex');
+
+$('#log').append('<p>' + Date.now() + ': Got secret, sending pubkey to server </p>');
+$.post('/diffie-hellman/finish', { clientKey: sessionStorage['clientPubKey'], serverKey: sessionStorage['serverPubKey'] }).done(function (result) {
+    if (result && result.success)
+        $('#log').append('<p>' + Date.now() + ': Handshake complete! Ready for HMAC</p>');
+    else
+        $('#log').append('<p>' + Date.now() + ': Handshake failed to complete, check server logs </p>');
+});
---
+++ diffie-hellman-express/routes/diffie-hellman-server.js
@@ -0,0 +1,33 @@
+﻿var crypto = require('crypto');
+
+var DiffieHellman = {};
+DiffieHellman.keys = {};
+
+DiffieHellman.Start = function (req, res, next) {
+    var keyInfo = {};
+    var dh = crypto.createDiffieHellman(2048);
+    keyInfo.key = dh.generateKeys('hex');
+    keyInfo.prime = dh.getPrime('hex');
+    keyInfo.generator = dh.getGenerator('hex');
+    DiffieHellman.keys[keyInfo.key] = {dh: dh};
+    res.json(keyInfo);
+    res.status(200);
+    res.end();
+};
+
+DiffieHellman.Finish = function (req, res, next) {
+    var serverKey = req.body['serverKey'];
+    var clientKey = req.body['clientKey'];
+    if (serverKey && clientKey) {
+        secret = DiffieHellman.keys[serverKey].dh.computeSecret(clientKey);
+        DiffieHellman.keys[serverKey].secret = secret;
+        DiffieHellman.keys[serverKey].expiry = Date.now() + (60 * 60 * 1000);
+        res.json({ 'success': true });
+    }
+    else
+        res.json({ 'success': false });
+    res.status(200);
+    res.end();
+};
+
+module.exports = DiffieHellman;
